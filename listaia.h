509fe7c06fe01650b50360f9388ab118ae069c86
Manuel J. Corbacho <m.j.corbacho@hotmail.com>
2018-09-26 00:23:54 +0200

    Add files via upload
    Provided

---
+++ listaia.h
@@ -0,0 +1,62 @@
+/**************************************************************/
+/* 		    LISTAIA.C                                         */
+/*						                                      */
+/* Asignatura: Inteligencia Artificial                        */
+/* Funciones para manejar listas estáticas de acuerdo         */
+/*           a las necesidades de la estrategia               */
+/*           de busqueda                                      */
+/**************************************************************/
+
+
+#define MAXI 3000
+
+#ifndef _tElemento_
+#define _tElemento_
+   typedef int tElemento;
+#endif
+
+
+#define ERROR(s) printf("%s\n", s);
+
+
+#ifndef _LISTAIA_H_
+#define _LISTAIA_H_
+   typedef struct {
+      tElemento *elementos;
+      int Lmax;  //longitud máxima de la lista
+      int Nelem; //número de elmentos
+      int inicio, fin;
+   }tLista;
+
+   typedef tLista *Lista;
+
+   Lista CrearLista(int TamMax);
+   int ListaVacia (Lista C);
+   int ListaLlena(Lista C);
+
+   //Pre: Lista C no está llena
+   //Post:Devuelve el elemento primero de la lista
+   void InsertarPrimero(tElemento *x, Lista C);
+
+  //Pre: Lista C no está llena
+  //Post:Devuelve el ultimo elemento de la lista
+   void InsertarUltimo(tElemento *x, Lista C);
+
+   //Pre: Lista C no está vacía, i: es un índice entre 0 y TamMax
+   //Post:Devuelve el elemento i-ésimo de la lista C
+   tElemento *ExtraerElem(Lista C,int i);
+
+   //Pre: Lista C no está vacía
+   //Post:Devuelve el primer elemento de la lista C
+   tElemento *ExtraerPrimero(Lista C);
+
+   //Pre: Lista L1 no está vacía
+   //Post: Devuelve L1=L1+L2
+   Lista Concatenar(Lista L1,Lista L2);
+
+   void EliminarPrimero(Lista C);
+
+   void DestruirLista(Lista C);
+
+
+#endif
---
+++ nodo.h
@@ -0,0 +1,25 @@
+/*******************************************/
+/* 		    NODO.H                         */
+/*						                   */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+
+#ifndef _tElemento_
+#define _tElemento_
+   typedef struct NodoBusqueda{
+      tEstado *estado;
+      struct NodoBusqueda *padre;
+      unsigned operador;
+      int costeCamino;
+      int profundidad;
+      int valHeuristica;
+   } tNodoBusqueda;
+
+   typedef tNodoBusqueda tElemento;
+#endif
+
+
+
+
+
